595037a3795786231598def06b0419b9ea5d5720
Philip <Philips.abrahamsson@gmail.com>
2020-06-23 21:23:09 +0200

    Added loadbar,ramen,new frontpage.

--- homepageJs.js
+++ homepageJs.js
@@ -6,31 +6,41 @@ window.onload = function() {
 
 }
 
+window.onscroll = function(){this.myFunction();}
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+  }
+
 function QuoteFrom() {
-    return `<h4>
-    Took this picture in 2018 enjoying an Irish coffee while trying to understand the C# language. It was
-    at the very start of my journey to become a Software Developer.</h4><Br> 
-    <h1>Philip Abrahamsson<Br> <i>Aspiring Software Developer<i></h1>`
+    return `
+   <h1> Welcome to my homepage.<h1>
+    <h4>I am currently studying in Linköping to become a software developer.
+    This page is made as a project to learn more about Html and Css. With a little bit of Javascript.  </h4> 
+    <h1>Philip Abrahamsson<Br> <i>Aspiring software developer<i></h1><Br>`
 }
 
 function AboutMe() {
-    return `<h2>Aspiring Software Developer!</h2>
+    return `<h2>Aspiring software developer!</h2>
     I have been working within the Graphical business for about ten years, 
-    as a printmaker. But today I am aiming to become something else.
+    as a printmaker. But today I am aiming to become something else.<Br>
 
-    Since early in 2018 I have taken courses to reach my new life goal.
     In August 2019 I finally became a student to become a Software Developer, one step 
     closer to reaching my goal.<Br><Br> 
 
-    I like doing small projects as in this homepage, but also in other coding languages.
-    So to become even better and increase my understanding of html and css i made this homepage project.
+    I like doing projects as in this homepage, but also in other coding languages.
+    So to become even better and increase my understanding of html and css i made this homepage.
     More code projects can be found on my GitHub link in the upper right corner.  <Br><Br>
-    Projects not including any coding happens as well. For example, we just made a huge renovation 
-    project at our home, fixing floors, ceiling and walls.
-    As a Substitute in our housing society I hold an ongoing project to implement fibre 
-    into the two houses of our society. <Br><Br> Another side project I have is to help my partner
-    with her company. So to help her I made a homepage with Wordpress and took a small course
-    about SEO(search engine optimization).More about that later on the page.<Br><Br>
+    Currently i'm working with a new Wordpress homepage with a friend, where i fix the style with
+    css. And some minor functions with php. <Br>
+    I am also working on a solo project with Xamarin and C#. Just to learn some more about how to consume API's. For fun
+    it also includes a game.
+    It will be up soon on Github.
+     <Br><Br> Another old project I have is to help my partner
+    with her company. So to help her I made a <a href="https://beccastattooportfolio.com/">homepage</a> with Wordpress and took a small course
+    about SEO(search engine optimization).<Br><Br>
     Well that is a little bit about my projects. Up next is my competence level.
 
     `
@@ -50,9 +60,10 @@ function Enjoying() {
     weights, listen to some nice music, just makes you feel good. <Br><Br>
 
     Gaming is something I have done since the 8bit Nintendo. Nowadays I use the Ps4, but mostly
-    I am at the PC. (PCM)<Br><Br><Br><Br>
+    I am at the PC.<Br><Br><Br><Br>
         `
 }
+
 
 function Footer() {
     return `<h2>Thanks for checking out my page</h2><Br>
@@ -60,16 +71,20 @@ function Footer() {
     <Br><Br>
      
     Dont miss my Wordpress page &#10547; <a href="https://beccastattooportfolio.com/">My Wordpress homepage</a>! 
-    <Br><Br> 
+    <Br>
     `
 }
+var count = new Array();
 
-// function AnimateBar(bar){
-//     bar.style.width = "55%"; In progress
-//     console.log("scrolling");
-// }
+function AnimateBar(){
+    var showInfo = document.getElementById("bars").querySelectorAll(".Competence");
+    var y = window.scrollY;
+    console.log(y);
+    
+        //childNode.style.width = "80%";
+}
 
-var showInfo = document.getElementById("info");
+//var showInfo = document.getElementById("info");
 var count = new Array();
 
 function ExplainBar(bar){
@@ -77,20 +92,7 @@ function ExplainBar(bar){
    // showInfo.style.backgroundImage = "url(pics/testCode.png)";
     // showInfo.style.backgroundColor = "#1E1E1E";
     // showInfo.style.color = "#CE9178";
-    // if(bar.classList.contains("CSharp")){showInfo.style.boxShadow = "2px 2px 5px 2px #9CDCFE";showInfo.innerHTML = "I do enjoy writing in C#. One goal is to become an expert.<Br> The whole .net world is very interesting.<Br> A Long road awaits.";}
-    // else if(bar.classList.contains("Python")){showInfo.style.boxShadow = "2px 2px 5px 2px #9CDCFE";showInfo.innerHTML = "I like Python and would really like to call myself competent of it one day. The struggle with the indentations are something else.";}
-    // else if(bar.classList.contains("Html")){showInfo.style.boxShadow = "2px 2px 5px 2px #9CDCFE";showInfo.innerHTML = "Html is something i like to learn more about. Thats why im coding this right now.";}
-    // else if(bar.classList.contains("Css")){showInfo.style.boxShadow = "2px 2px 5px 2px #9CDCFE";showInfo.innerHTML = "Same here as with the Html language.";}
-    // else if(bar.classList.contains("Js")){showInfo.style.boxShadow = "2px 2px 5px 2px #9CDCFE";showInfo.innerHTML = "Have not used this as much as I would like to. Even thought this right here is made by it.";}
-    // else if(bar.classList.contains("Xaml")){showInfo.style.boxShadow = "2px 2px 5px 2px #9CDCFE";showInfo.innerHTML = "This reminds me of Html.<Br> I have used it while coding UWP and Xamarin applications. I like it!";}
-    // else if(bar.classList.contains("Git")){showInfo.style.boxShadow = "2px 2px 5px 2px #9CDCFE";showInfo.innerHTML = "I try to use Git as much as I can. Even in my own small projects. Such a great tool. Mostly used with GitHub.";}
-    // else if(bar.classList.contains("Vs")){showInfo.style.boxShadow = "2px 2px 5px 2px #9CDCFE";showInfo.innerHTML = "Visual Studio and Visual Studio Code are the compilers i use.";}
-    // else if(bar.classList.contains("Wp")){showInfo.style.boxShadow = "2px 2px 5px 2px #CE9178";showInfo.innerHTML = "Using Wordpress for a tattoo artist(my girlfriend). Link to that page at the bottom of this page.";}
-    // else if(bar.classList.contains("Seo")){showInfo.style.boxShadow = "2px 2px 5px 2px #CE9178";showInfo.innerHTML = "Took a short education about search engine optimisation.<Br> Used that knowledge on the Wordpress page. ";}
-    // else if(bar.classList.contains("Novice")){showInfo.style.boxShadow = "2px 2px 5px 2px #9CDCFE"; showInfo.style.color = "#9CDCFE"; showInfo.innerHTML = "This level is set after experience from coding at home and school. No workplace exeperience yet.";}
-    // else if(bar.classList.contains("AdvBeg")){showInfo.style.boxShadow = "2px 2px 5px 2px #CE9178"; showInfo.style.color = "#CE9178"; showInfo.innerHTML = `This level is set after how much workplace experience gained.<Br> Or how much "real life" experience gained.`;}
-    // else if(bar.classList.contains("Comp")){showInfo.style.boxShadow = "2px 2px 5px 2px #D7BA7D"; showInfo.style.color = "#D7BA7D"; showInfo.innerHTML = `This level is also set after how much workplace experience gained. But within this level atleast 2 years of experience. Combined with personal projects.`;}
-    // else if(bar.classList.contains("Expert")){showInfo.style.boxShadow = "2px 2px 5px 2px #6A9955"; showInfo.style.color = "#6A9955"; showInfo.innerHTML = `This level is also set after how much workplace experience gained. But within this level atleast 5+ years of experience. Combined with personal projects.`;}
+    // if(bar.classList.contains("CSharp")){showInfo.style.boxShadow = "2px 2px 5px 2px #9CDCFE";showInfo.innerHTML = ;}
 
     // console.log(bar.style.backgroundColor);
 
@@ -107,4 +109,73 @@ function NormalBar(bar){
     //     else showInfo.innerHTML = `↑Hoover over level bars above for information.<Br><Br>←Hoover over any competence bar for information.<Br><Br> You just checked<Br> <b>${bar.classList}</b> bar.`;
     // console.log(count.length);
   
+}
+/**This function creates new elements.
+ * Elements are styled from the class "Boxes".
+ * Each element are appended as childs to the "one"
+ * element. 
+ */
+var stopRamen = true;
+function getMoreBoxes() {
+    deleteAll();
+    if(!stopRamen){return stopRamen=true;}
+    else if(stopRamen)
+    {
+        stopRamen=false;
+        var move = 150;
+        var score = 0;
+        var myBoxWithBoxes = new Array();
+        for (i = 0; i < 10; i++) {
+            move += 50;
+            let box = document.createElement("div");
+            box.className = "Boxes";
+            box.id = "clones";
+            box.innerHTML = "&#127836;";
+            box.style.fontSize = "50px"
+            
+            move += Math.random() * 60;
+            box.style.left = move + 'px';
+            myBoxWithBoxes.push(box);
+            console.log(myBoxWithBoxes)
+        }
+        for (boxes in myBoxWithBoxes) {
+            console.log(boxes);
+            console.log(myBoxWithBoxes.length);
+            let name = myBoxWithBoxes[boxes].id = `clone${boxes}`;
+            document.getElementById("ramen").appendChild(myBoxWithBoxes[boxes]);
+            console.log(myBoxWithBoxes[boxes]);
+            console.log("is this the name ?" + name);
+            moveBox(name);
+        }
+    }
+}
+/**This function is used to move elements.
+ * It will go on forever.
+ */
+function moveBox(box) {
+    let boxPosition = 1;
+    var theBox = document.getElementById(box);
+    setInterval(moveFrame, Math.random() * 15);
+
+    function moveFrame() {
+        if (boxPosition == 820) {
+            boxPosition = 1;
+        } else {
+            boxPosition++;
+            theBox.style.top = boxPosition + 'px';
+        }
+    }
+}
+/**This function is used to delete elements.
+ * In this case it deletes all the cloned elements 
+ * creted for getMoreBoxes() function.
+ */
+function deleteAll() {
+    for (i = 0; i < 10; i++) {
+        var myElements = document.getElementById(`clone${i}`);
+        console.log(`clone${i}`)
+        if (myElements != null) {
+            myElements.remove();
+        }
+    }
 }
